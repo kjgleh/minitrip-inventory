be626da5cd09a8f3d11626ab8163700d64fa70a5
김정근 <gogoogjg@tripstore.kr>
2024-07-16 18:19:27 +0900

    프로젝트 초기화

---
+++ src/test/kotlin/com/example/minitripinventory/MinitripInventoryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.minitripinventory
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MinitripInventoryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
